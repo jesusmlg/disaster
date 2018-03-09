7b5a1e7c6a7cbed536a0fc146ed3090becdbc6bf
jesusmlg <jesusmlg@gmail.com>
2018-03-09 11:36:43 +0100

    add margin to edit button

--- resources/views/note/edit.blade.php
+++ resources/views/note/edit.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-	<h2>Edit Note (#{{ $note->id }}) {!! Form::open(['method' => 'DELETE','route' => ['note_destroy', $note->id],'onsubmit' => 'return confirm("are you sure ?")','style'=>'display:inline; float:right;']) !!}
+	<h2>
+		Edit Note (#{{ $note->id }}) {!! Form::open(['method' => 'DELETE','route' => ['note_destroy', $note->id],'onsubmit' => 'return confirm("are you sure ?")','style'=>'display:inline; float:right;']) !!}
 		{!! Form::submit('Delete', ['class' => 'btn btn-danger btn-lg']) !!}
-	{!! Form::close() !!}</h2>
-	
-	{!! Form::model($note,['route' => ['note_update',$note->id], 'files' => true]) !!}
-		@include('note._form') 
 	{!! Form::close() !!}
+	</h2>
+	
+	<div style="margin-top:40px">
+		{!! Form::model($note,['route' => ['note_update',$note->id], 'files' => true]) !!}
+			@include('note._form') 
+		{!! Form::close() !!}
+	</div>
+	
 @endsection
